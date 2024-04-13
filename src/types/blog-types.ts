5d425c8eaed65cf5326aaa3172797a2ae9e2729e
belij2111 <belijvalerij2111@gmail.com>
2024-04-13 22:56:51 +0300

    feat: Add types for blog and post

---
+++ src/types/blog-types.ts
@@ -0,0 +1,11 @@
+export type OutputBlogType = {
+    id: string,
+    name: string,
+    description: string,
+    websiteUrl: string
+}
+export type InputBlogType = {
+    name: string,
+    description: string,
+    websiteUrl: string
+}
---
+++ src/types/post-types.ts
@@ -0,0 +1,14 @@
+export type OutputPostType = {
+    id: string,
+    title: string,
+    shortDescription: string,
+    content: string,
+    blogId: string,
+    blogName: string
+}
+export type InputPostType = {
+    title: string,
+    shortDescription: string,
+    content: string,
+    blogId: string
+}

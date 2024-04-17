f6a99a796c7ddd6a661883a3c822c8ff41277e2e
belij2111 <belijvalerij2111@gmail.com>
2024-04-17 22:26:58 +0300

    fix: Fix the request parameters

--- src/repositories/blogs-repozitory.ts
+++ src/repositories/blogs-repozitory.ts
@@ -1,8 +1,8 @@
-import {db} from "../db/db"
+import {blogsDB} from "../db/db"
 
 export const blogsRepository = {
     getBlogs(name: string | null | undefined) {
-        let filteredBlogs = db.blogs
+        let filteredBlogs = blogsDB.blogs
         if (name) {
             filteredBlogs = filteredBlogs.filter(e => e.name.indexOf(name) > 1)
         }

bd2ad168dd5b109cc7652a88e3d6970674cb6fc8
belij2111 <belijvalerij2111@gmail.com>
2024-04-17 22:30:56 +0300

    test: Add a test for a get request

--- __tests__/blogs.e2e.test.ts
+++ __tests__/blogs.e2e.test.ts
@@ -1,14 +1,24 @@
 import {setDB} from "../src/db/db";
 import {HTTP_STATUSES, SETTINGS} from "../src/settings";
 import {req} from "./test-helpers";
+import {blogDataset1} from "./blogs-datasets";
 
 describe('/blogs', () => {
-    it('should get all blogs', async () => {
+    it(`should get empty array`, async () => {
         setDB()
         const res = await req
             .get(SETTINGS.PATH.BLOGS)
             .expect(HTTP_STATUSES.OK_200)
         console.log(res.body)
         expect(res.body.length).toBe(0)
+    });
+    it(`should get not empty array`, async () => {
+        setDB(blogDataset1)
+        const res = await req
+            .get(SETTINGS.PATH.BLOGS)
+            .expect(HTTP_STATUSES.OK_200)
+        console.log(res.body)
+        expect(res.body.length).toBe(1)
+        expect(res.body[0]).toEqual(blogDataset1.blogs[0])
     });
 })

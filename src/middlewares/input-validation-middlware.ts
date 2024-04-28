c8a53a59b54b89b25fa99376aed0d6d3c9be9a63
belij2111 <belijvalerij2111@gmail.com>
2024-04-28 17:25:24 +0300

    fix: Fix incorrect validation of input data for blogs

--- src/middlewares/input-validation-middlware.ts
+++ src/middlewares/input-validation-middlware.ts
@@ -1,16 +1,16 @@
 import {NextFunction, Request, Response} from "express";
-import {validationResult} from "express-validator"
+import {FieldValidationError, validationResult} from "express-validator"
 import {HTTP_STATUSES} from "../settings";
 
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         res
-            .status(HTTP_STATUSES.NOT_FOUND_404)
+            .status(HTTP_STATUSES.BAD_REQUEST_400)
             .json({
-                errorsMessages: errors.array().map(error => ({
+                errorsMessages: (errors.array({onlyFirstError:true}) as FieldValidationError[]).map(error => ({
                     message: error.msg,
-                    field: error.type
+                    field: error.path
                 }))
             })
         return
